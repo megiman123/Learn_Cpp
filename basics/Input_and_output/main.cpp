0c3c1aa64f4902aa0e39caf5adf099b90cc0dfb0
megiman123 <tiwarishambhav625@gmail.com>
2025-09-11 15:00:14 +0530

    The basic input and output file

---
+++ basics/Input_and_output/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // basic input
+    cout << "Hello, World!" << endl;//cout - character output , endl - end line
+
+    //basic input
+    cout << "enter your age : "<<endl; 
+    int age;
+    cin >> age; //cin - character input
+    cout << "You are " << age << " years old." << endl;//printing the final statement
+    
+    return 0;
+}
